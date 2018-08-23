5360450b4d9f994f9b3cd2d8cbe52fa44feccf7b
Justineo <justice360@gmail.com>
2018-08-23 23:56:56 +0800

    chore: remove sourcemap from demo

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -33,7 +33,7 @@ var webpackConfig = merge(baseWebpackConfig, {
       compress: {
         warnings: false
       },
-      sourceMap: true
+      sourceMap: false
     }),
     // extract css into its own file
     new ExtractTextPlugin({
