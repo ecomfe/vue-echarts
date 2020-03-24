a2cfd239f1193bdbf96f8cce2341f6946260b79f
tiaoyuw <tiaoyuw@ca.ibm.com>
2020-03-24 06:04:58 -0400

    Add getinstance method

--- src/components/ECharts.vue
+++ src/components/ECharts.vue
@@ -10,12 +10,12 @@
 </style>
 
 <script>
-import echarts from 'echarts/lib/echarts'
-import debounce from 'lodash/debounce'
-import { addListener, removeListener } from 'resize-detector'
+import echarts from "echarts/lib/echarts";
+import debounce from "lodash/debounce";
+import { addListener, removeListener } from "resize-detector";
 
-const INIT_TRIGGERS = ['theme', 'initOptions', 'autoresize']
-const REWATCH_TRIGGERS = ['manualUpdate', 'watchShallow']
+const INIT_TRIGGERS = ["theme", "initOptions", "autoresize"];
+const REWATCH_TRIGGERS = ["manualUpdate", "watchShallow"];
 
 export default {
   props: {
@@ -27,123 +27,129 @@ export default {
     watchShallow: Boolean,
     manualUpdate: Boolean
   },
-  data () {
+  data() {
     return {
       lastArea: 0
-    }
+    };
   },
   watch: {
-    group (group) {
-      this.chart.group = group
+    group(group) {
+      this.chart.group = group;
     }
   },
   methods: {
     // provide an explicit merge option method
-    mergeOptions (options, notMerge, lazyUpdate) {
+    mergeOptions(options, notMerge, lazyUpdate) {
       if (this.manualUpdate) {
-        this.manualOptions = options
+        this.manualOptions = options;
       }
 
       if (!this.chart) {
-        this.init(options)
+        this.init(options);
       } else {
-        this.delegateMethod('setOption', options, notMerge, lazyUpdate)
+        this.delegateMethod("setOption", options, notMerge, lazyUpdate);
       }
     },
     // just delegates ECharts methods to Vue component
     // use explicit params to reduce transpiled size for now
-    appendData (params) {
-      this.delegateMethod('appendData', params)
+    appendData(params) {
+      this.delegateMethod("appendData", params);
     },
-    resize (options) {
-      this.delegateMethod('resize', options)
+    getChartInstance() {
+      return this.chart;
     },
-    dispatchAction (payload) {
-      this.delegateMethod('dispatchAction', payload)
+    resize(options) {
+      this.delegateMethod("resize", options);
     },
-    convertToPixel (finder, value) {
-      return this.delegateMethod('convertToPixel', finder, value)
+    dispatchAction(payload) {
+      this.delegateMethod("dispatchAction", payload);
     },
-    convertFromPixel (finder, value) {
-      return this.delegateMethod('convertFromPixel', finder, value)
+    convertToPixel(finder, value) {
+      return this.delegateMethod("convertToPixel", finder, value);
     },
-    containPixel (finder, value) {
-      return this.delegateMethod('containPixel', finder, value)
+    convertFromPixel(finder, value) {
+      return this.delegateMethod("convertFromPixel", finder, value);
     },
-    showLoading (type, options) {
-      this.delegateMethod('showLoading', type, options)
+    containPixel(finder, value) {
+      return this.delegateMethod("containPixel", finder, value);
     },
-    hideLoading () {
-      this.delegateMethod('hideLoading')
+    showLoading(type, options) {
+      this.delegateMethod("showLoading", type, options);
     },
-    getDataURL (options) {
-      return this.delegateMethod('getDataURL', options)
+    hideLoading() {
+      this.delegateMethod("hideLoading");
     },
-    getConnectedDataURL (options) {
-      return this.delegateMethod('getConnectedDataURL', options)
+    getDataURL(options) {
+      return this.delegateMethod("getDataURL", options);
     },
-    clear () {
-      this.delegateMethod('clear')
+    getConnectedDataURL(options) {
+      return this.delegateMethod("getConnectedDataURL", options);
     },
-    dispose () {
-      this.delegateMethod('dispose')
+    clear() {
+      this.delegateMethod("clear");
     },
-    delegateMethod (name, ...args) {
+    dispose() {
+      this.delegateMethod("dispose");
+    },
+    delegateMethod(name, ...args) {
       if (!this.chart) {
-        this.init()
+        this.init();
       }
-      return this.chart[name](...args)
+      return this.chart[name](...args);
     },
-    delegateGet (methodName) {
+    delegateGet(methodName) {
       if (!this.chart) {
-        this.init()
+        this.init();
       }
-      return this.chart[methodName]()
+      return this.chart[methodName]();
     },
-    getArea () {
-      return this.$el.offsetWidth * this.$el.offsetHeight
+    getArea() {
+      return this.$el.offsetWidth * this.$el.offsetHeight;
     },
-    init (options) {
+    init(options) {
       if (this.chart) {
-        return
+        return;
       }
 
-      const chart = echarts.init(this.$el, this.theme, this.initOptions)
+      const chart = echarts.init(this.$el, this.theme, this.initOptions);
 
       if (this.group) {
-        chart.group = this.group
+        chart.group = this.group;
       }
 
-      chart.setOption(options || this.manualOptions || this.options || {}, true)
+      chart.setOption(
+        options || this.manualOptions || this.options || {},
+        true
+      );
 
       Object.keys(this.$listeners).forEach(event => {
-        const handler = this.$listeners[event]
+        const handler = this.$listeners[event];
 
-        if (event.indexOf('zr:') === 0) {
-          chart.getZr().on(event.slice(3), handler)
+        if (event.indexOf("zr:") === 0) {
+          chart.getZr().on(event.slice(3), handler);
         } else {
-          chart.on(event, handler)
+          chart.on(event, handler);
         }
-      })
+      });
 
       if (this.autoresize) {
-        this.lastArea = this.getArea()
+        this.lastArea = this.getArea();
         this.__resizeHandler = debounce(
           () => {
             if (this.lastArea === 0) {
               // emulate initial render for initially hidden charts
-              this.mergeOptions({}, true)
-              this.resize()
-              this.mergeOptions(this.options || this.manualOptions || {}, true)
+              this.mergeOptions({}, true);
+              this.resize();
+              this.mergeOptions(this.options || this.manualOptions || {}, true);
             } else {
-              this.resize()
+              this.resize();
             }
-            this.lastArea = this.getArea()
+            this.lastArea = this.getArea();
           },
           100,
           { leading: true }
-        )
-        addListener(this.$el, this.__resizeHandler)
+        );
+        addListener(this.$el, this.__resizeHandler);
       }
 
       Object.defineProperties(this, {
@@ -153,43 +159,43 @@ export default {
         width: {
           configurable: true,
           get: () => {
-            return this.delegateGet('getWidth')
+            return this.delegateGet("getWidth");
           }
         },
         height: {
           configurable: true,
           get: () => {
-            return this.delegateGet('getHeight')
+            return this.delegateGet("getHeight");
           }
         },
         isDisposed: {
           configurable: true,
           get: () => {
-            return !!this.delegateGet('isDisposed')
+            return !!this.delegateGet("isDisposed");
           }
         },
         computedOptions: {
           configurable: true,
           get: () => {
-            return this.delegateGet('getOption')
+            return this.delegateGet("getOption");
           }
         }
-      })
+      });
 
-      this.chart = chart
+      this.chart = chart;
     },
-    initOptionsWatcher () {
+    initOptionsWatcher() {
       if (this.__unwatchOptions) {
-        this.__unwatchOptions()
-        this.__unwatchOptions = null
+        this.__unwatchOptions();
+        this.__unwatchOptions = null;
       }
 
       if (!this.manualUpdate) {
         this.__unwatchOptions = this.$watch(
-          'options',
+          "options",
           (val, oldVal) => {
             if (!this.chart && val) {
-              this.init()
+              this.init();
             } else {
               // mutating `options` will lead to merging
               // replacing it with new reference will lead to not merging
@@ -198,81 +204,81 @@ export default {
               // will trigger `this.chart.setOption(val, true)
               // `this.options.title.text = 'Trends'`
               // will trigger `this.chart.setOption(val, false)`
-              this.chart.setOption(val, val !== oldVal)
+              this.chart.setOption(val, val !== oldVal);
             }
           },
           { deep: !this.watchShallow }
-        )
+        );
       }
     },
-    destroy () {
+    destroy() {
       if (this.autoresize) {
-        removeListener(this.$el, this.__resizeHandler)
+        removeListener(this.$el, this.__resizeHandler);
       }
-      this.dispose()
-      this.chart = null
+      this.dispose();
+      this.chart = null;
     },
-    refresh () {
+    refresh() {
       if (this.chart) {
-        this.destroy()
-        this.init()
+        this.destroy();
+        this.init();
       }
     }
   },
-  created () {
-    this.initOptionsWatcher()
+  created() {
+    this.initOptionsWatcher();
 
     INIT_TRIGGERS.forEach(prop => {
       this.$watch(
         prop,
         () => {
-          this.refresh()
+          this.refresh();
         },
         { deep: true }
-      )
-    })
+      );
+    });
 
     REWATCH_TRIGGERS.forEach(prop => {
       this.$watch(prop, () => {
-        this.initOptionsWatcher()
-        this.refresh()
-      })
-    })
+        this.initOptionsWatcher();
+        this.refresh();
+      });
+    });
   },
-  mounted () {
+  mounted() {
     // auto init if `options` is already provided
     if (this.options) {
-      this.init()
+      this.init();
     }
   },
-  activated () {
+  activated() {
     if (this.autoresize) {
-      this.chart && this.chart.resize()
+      this.chart && this.chart.resize();
     }
   },
-  destroyed () {
+  destroyed() {
     if (this.chart) {
-      this.destroy()
+      this.destroy();
     }
   },
-  connect (group) {
-    if (typeof group !== 'string') {
-      group = group.map(chart => chart.chart)
+  connect(group) {
+    if (typeof group !== "string") {
+      group = group.map(chart => chart.chart);
     }
-    echarts.connect(group)
+    echarts.connect(group);
   },
-  disconnect (group) {
-    echarts.disConnect(group)
+  disconnect(group) {
+    echarts.disConnect(group);
   },
-  getMap (mapName) {
-    return echarts.getMap(mapName)
+  getMap(mapName) {
+    return echarts.getMap(mapName);
   },
-  registerMap (mapName, geoJSON, specialAreas) {
-    echarts.registerMap(mapName, geoJSON, specialAreas)
+  registerMap(mapName, geoJSON, specialAreas) {
+    echarts.registerMap(mapName, geoJSON, specialAreas);
   },
-  registerTheme (name, theme) {
-    echarts.registerTheme(name, theme)
+  registerTheme(name, theme) {
+    echarts.registerTheme(name, theme);
   },
   graphic: echarts.graphic
-}
+};
 </script>
