d89e0ee9b3fe0939e26d4fcea8a7296280b75161
Justineo <justice360@gmail.com>
2021-03-08 19:21:59 +0800

    chore: add postinstall script to bail out type check for vue 2

---
+++ scripts/postinstall.js
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
+const fs = require("fs");
+
+const packageFile = path.resolve(__dirname, "../package.json");
+
+const typesPaths = {
+  3: "dist/index.d.ts",
+  2: "dist/index.vue2.d.ts"
+};
+
+function switchVersion(version) {
+  const typesPath = typesPaths[version];
+  const package = JSON.parse(fs.readFileSync(packageFile, "utf-8"));
+  const current = package.types || package.typings;
+  if (typesPath !== current) {
+    fs.writeFileSync(JSON.stringify(package, null, "  "));
+  }
+  console.log(`[vue-echarts] Switched to Vue ${version} environment.`);
+}
+
+function loadVue() {
+  try {
+    return require("vue");
+  } catch (e) {
+    return null;
+  }
+}
+
+const Vue = loadVue();
+
+// Align the process with vue-demi
+if (!Vue || typeof Vue.version !== "string") {
+  console.warn(
+    '[vue-echarts] Vue is not found. Please run "npm install vue" to install.'
+  );
+} else if (Vue.version.startsWith("3.")) {
+  switchVersion(3);
+} else if (Vue.version.startsWith("2.")) {
+  switchVersion(2);
+} else {
+  console.warn(`[vue-echarts] Vue version v${Vue.version} is not supported.`);
+}
---
+++ src/index.vue2.d.ts
@@ -0,0 +1,9 @@
+declare const LOADING_OPTIONS_KEY = "ecLoadingOptions";
+declare const THEME_KEY = "ecTheme";
+declare const INIT_OPTIONS_KEY = "ecInitOptions";
+declare const UPDATE_OPTIONS_KEY = "ecUpdateOptions";
+
+declare const _default: any;
+
+export default _default;
+export { INIT_OPTIONS_KEY, LOADING_OPTIONS_KEY, THEME_KEY, UPDATE_OPTIONS_KEY };
