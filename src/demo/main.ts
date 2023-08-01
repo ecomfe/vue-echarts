e48067c5d3e4e497c0da0890e052fd18224b71cc
Justineo <justice360@gmail.com>
2023-08-01 15:19:20 +0800

    chore: add vercel analytics

--- src/demo/main.ts
+++ src/demo/main.ts
@@ -1,6 +1,9 @@
+import { inject } from "@vercel/analytics";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import Demo from "./Demo.vue";
+
+inject();
 
 const pinia = createPinia();
 const app = createApp(Demo);
