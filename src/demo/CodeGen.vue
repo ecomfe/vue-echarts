70c32603d4dc222b991e2d9828a0d0f6abd75b9d
Justineo <justice360@gmail.com>
2023-10-31 19:19:24 +0800

    fix: revert to esbuild-wasm@0.19.2

--- src/demo/CodeGen.vue
+++ src/demo/CodeGen.vue
@@ -81,6 +81,42 @@ onMounted(async () => {
 
   initializing.value = false;
 
+  optionCode.value = `{
+  title: {
+    text: 'Referer of a Website',
+    subtext: 'Fake Data',
+    left: 'center'
+  },
+  tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    orient: 'vertical',
+    left: 'left'
+  },
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: '50%',
+      data: [
+        { value: 1048, name: 'Search Engine' },
+        { value: 735, name: 'Direct' },
+        { value: 580, name: 'Email' },
+        { value: 484, name: 'Union Ads' },
+        { value: 300, name: 'Video Ads' }
+      ],
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      }
+    }
+  ]
+}`;
+
   await nextTick();
 
   source.value?.focus();
@@ -238,6 +274,7 @@ onBeforeUnmount(() => {
           "
           :disabled="initializing"
           autofocus
+          spellcheck="false"
         ></textarea>
         <div class="import-code" @copy="trackCopy('system')">
           <code-highlight
