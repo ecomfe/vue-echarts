3c7ff95331afced6da7d489f386b33f6ec87a840
Justineo <justice360@gmail.com>
2024-05-11 15:36:34 +0800

    refactor!: remove resize-detector and update vue deps to 2.7+

--- src/composables/autoresize.ts
+++ src/composables/autoresize.ts
@@ -1,10 +1,5 @@
 import { watch, type Ref, type PropType } from "vue-demi";
 import { throttle } from "echarts/core";
-import {
-  addListener,
-  removeListener,
-  type ResizeCallback
-} from "resize-detector";
 import { type EChartsType } from "../types";
 
 type AutoresizeProp =
@@ -19,28 +14,50 @@ export function useAutoresize(
   autoresize: Ref<AutoresizeProp | undefined>,
   root: Ref<HTMLElement | undefined>
 ): void {
-  let resizeListener: ResizeCallback | null = null;
+  watch(
+    [root, chart, autoresize],
+    ([root, chart, autoresize], _, onCleanup) => {
+      let ro: ResizeObserver | null = null;
 
-  watch([root, chart, autoresize], ([root, chart, autoresize], _, cleanup) => {
-    if (root && chart && autoresize) {
-      const autoresizeOptions = autoresize === true ? {} : autoresize;
-      const { throttle: wait = 100, onResize } = autoresizeOptions;
+      if (root && chart && autoresize) {
+        const { offsetWidth, offsetHeight } = root;
+        const autoresizeOptions = autoresize === true ? {} : autoresize;
+        const { throttle: wait = 100, onResize } = autoresizeOptions;
 
-      const callback = () => {
-        chart.resize();
-        onResize?.();
-      };
+        let initialResizeTriggered = false;
 
-      resizeListener = wait ? throttle(callback, wait) : callback;
-      addListener(root, resizeListener);
-    }
+        const callback = () => {
+          chart.resize();
+          onResize?.();
+        };
 
-    cleanup(() => {
-      if (root && resizeListener) {
-        removeListener(root, resizeListener);
+        const resizeCallback = wait ? throttle(callback, wait) : callback;
+
+        ro = new ResizeObserver(() => {
+          // We just skip ResizeObserver's initial resize callback if the
+          // size has not changed since the chart is rendered.
+          if (!initialResizeTriggered) {
+            initialResizeTriggered = true;
+            if (
+              root.offsetWidth === offsetWidth &&
+              root.offsetHeight === offsetHeight
+            ) {
+              return;
+            }
+          }
+          resizeCallback();
+        });
+        ro.observe(root);
       }
-    });
-  });
+
+      onCleanup(() => {
+        if (ro) {
+          ro.disconnect();
+          ro = null;
+        }
+      });
+    }
+  );
 }
 
 export const autoresizeProps = {
