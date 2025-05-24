eb11964945dcff10ac292b7d2e233212398a016b
Yue JIN <yuejin13@qq.com>
2025-05-24 20:17:38 +0800

    chore: remove browserslist and babel

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import legacy from "@vitejs/plugin-legacy";
-import postcssPresetEnv from "postcss-preset-env";
-import browserslist from "browserslist";
-import browserslistToEsbuild from "browserslist-to-esbuild";
+import postcssNested from "postcss-nested";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    legacy({
-      modernPolyfills: true,
-      renderLegacyChunks: false,
-      modernTargets: browserslist()
-    })
-  ],
-  build: { target: browserslistToEsbuild() },
+  plugins: [vue()],
   root: "./demo",
   server: {
     host: true
   },
   css: {
     postcss: {
-      plugins: [postcssPresetEnv()]
+      plugins: [postcssNested()]
     }
   }
 });
