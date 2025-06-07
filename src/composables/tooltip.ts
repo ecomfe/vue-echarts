978317eef9c50a3b84d734455ea14bbae09f786d
Yue JIN <yuejin13@qq.com>
2025-06-07 21:16:57 +0800

    feat: try createApp

--- src/composables/tooltip.ts
+++ src/composables/tooltip.ts
@@ -1,14 +1,17 @@
 import {
-  h,
-  render,
   defineComponent,
   Slot,
-  Fragment,
+  shallowRef,
+  createApp,
+  onUnmounted,
+  type App,
   type DefineComponent,
 } from "vue";
 
 export function createTooltipTemplate<Params extends object>() {
   let slot: Slot | undefined;
+  let app: App<Element> | undefined;
+  const props = shallowRef<Params>();
 
   const define = defineComponent({
     setup(_, { slots }) {
@@ -20,22 +23,33 @@ export function createTooltipTemplate<Params extends object>() {
     new (): { $slots: { default(_: Params): any } };
   };
 
-  const formatter = (params: Params): HTMLElement[] => {
+  const formatter = (params: Params) => {
+    props.value = params;
+
     if (!slot) {
       throw new Error(
         `[vue-echarts] Failed to find the definition of tooltip template`,
       );
     }
 
-    const container = document.createElement("div");
+    if (!app) {
+      app = createApp({
+        // root component is just a render function
+        render() {
+          // call the slot function with your props
+          // return slot!(props);
+          return slot!(props.value);
+        },
+      });
+      app.mount(document.createElement("div"));
+    }
 
-    const vnode = h(Fragment, null, slot(params));
-    // console.log(params);
-
-    render(vnode, container);
-
-    return Array.from(container.children) as HTMLElement[];
+    return app._container!.innerHTML;
   };
+
+  onUnmounted(() => {
+    app?.unmount();
+  });
 
   return { define, formatter };
 }
