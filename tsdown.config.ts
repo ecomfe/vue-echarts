8ed975e09b89c60c11b79b610196157a24504bbb
Yue JIN <40021217+kingyue737@users.noreply.github.com>
2025-08-05 12:15:21 +0800

    feat!: inject style via constructable `CSSStyleSheet` and remove CSP entry (#847)
    * chore: not inject inline css on server
    
    * feat!: remove csp entry
    
    * keep csp title in readme
    
    * chore: switch to rolldown and tsdown
    
    * update
    
    * dedupe
    
    * update according to review
    
    * emphasize "both" in csp section
    
    * load css with unplugin-raw
    
    * change tsdown entry

---
+++ tsdown.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "tsdown";
+import raw from "unplugin-raw/rollup";
+
+export default defineConfig([
+  {
+    entry: "src/index.ts",
+    platform: "browser",
+    sourcemap: true,
+    copy: ["src/style.css"],
+    plugins: [raw()],
+  },
+  {
+    entry: "src/global.ts",
+    outputOptions: {
+      file: "dist/index.min.js", // for unpkg/jsdelivr
+      dir: undefined,
+      format: "umd",
+      name: "VueECharts",
+      exports: "default",
+      globals: {
+        vue: "Vue",
+        echarts: "echarts",
+        "echarts/core": "echarts",
+      },
+    },
+    platform: "browser",
+    sourcemap: true,
+    minify: true,
+    dts: false,
+    plugins: [raw()],
+  },
+]);
