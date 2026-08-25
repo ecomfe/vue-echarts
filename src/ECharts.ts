2666754322473524746cb2d87c20e381d0580f2d
Justineo <justice360@gmail.com>
2026-08-25 23:11:17 +0800

    perf(lifecycle): release themed chart on cleanup

--- src/ECharts.ts
+++ src/ECharts.ts
@@ -242,6 +242,7 @@ export default /* @__PURE__ */ defineComponent({
       }
       isReady.value = false;
       lastSignature = undefined;
+      themedChart = undefined;
       graphicSlotApplied = false;
     }
 
