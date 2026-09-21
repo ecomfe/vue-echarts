f650d4b348c6d19143671cb0ffe0c185147d4a81
p-morel-92 <22473376+p-morel-92@users.noreply.github.com>
2026-09-21 09:54:55 +0200

    refactor(types): fold PublicComponent into existing types export

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ export interface VChartSlotsExtension {
 
 export default ECharts;
 export * from "./ECharts";
-export type { AutoResize, LoadingOptions } from "./types";
-export type { PublicComponent } from "./types";
+export type { AutoResize, LoadingOptions, PublicComponent } from "./types";
 export type { PublicMethods } from "./composables/api";
 export type { SlotsTypes } from "./composables/slot";
